37c667e2044b8e144686aec63d7506cc1cde5b8e
John Paulett <john@paulett.org>
2018-02-14 14:44:14 -0500

    Use the main login page instead of the admin login page for the admin.

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 from django.views.static import serve
 
 from . import views
@@ -10,7 +11,13 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^robots.txt$', views.robots),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+
+    # Use the normal login page, not the admin's login
+    url(r'^admin/login/$', RedirectView.as_view(
+        url=settings.LOGIN_URL, query_string=True
+    )),
     url(r'^admin/', admin.site.urls),
+
     url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
 
     # WARN: We use runserver to host the static content (not recommended)
